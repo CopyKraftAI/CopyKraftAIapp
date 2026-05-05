44ac7ac6313fe80154f505988449971f60583285
CopyKraftAI <julia@speaktoinspire.com>
2026-05-05 12:18:27 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': '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',
+      'x-api-key': process.env.ANTHROPIC_KEY,
       'anthropic-version': '2023-06-01'
     },
     body: JSON.stringify({
